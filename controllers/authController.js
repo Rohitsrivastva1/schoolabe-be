ad37694434306e4a6219dd51c04c1370174a7ecf
rohitsrivastva1 <rohit.managers@gmail.com>
2025-07-20 04:39:50 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 require("dotenv").config();
 const { getOTPEmailTemplate } = require('./emailTemplate');
-
+const { getLoginOTPEmailTemplate } = require('./emailTemplate');
 // Helper function to generate OTP
 const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
 const sendEmail = require("../utils/mailer"); // Import mailer
